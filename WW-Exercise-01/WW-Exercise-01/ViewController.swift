4cf03ab4f504327b8fa70e5f6a81334b8128b10d
Paul Newman <prnewman@gmail.com>
2016-07-13 14:35:48 -0400

    Added Exercise 1. Removed Main.storyboard.

---
+++ WW-Exercise-01/WW-Exercise-01/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  WW-Exercise-01
+//
+//  Created by Paul Newman on 7/13/16.
+//  Copyright © 2016 Weight Watchers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var label = UILabel()
+    var imageView = UIImageView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.greenColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
